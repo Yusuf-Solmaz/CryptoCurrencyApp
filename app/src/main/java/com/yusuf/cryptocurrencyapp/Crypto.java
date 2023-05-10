b72528f0881736c358a95cc8692f718757bdb4ee
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-05-10 21:12:31 +0300

    DAO

--- app/src/main/java/com/yusuf/cryptocurrencyapp/Crypto.java
+++ app/src/main/java/com/yusuf/cryptocurrencyapp/Crypto.java
@@ -1,7 +1,14 @@
 package com.yusuf.cryptocurrencyapp;
 
+
+import com.google.gson.annotations.SerializedName;
+
 public class Crypto {
 
+
+    @SerializedName("currency")
     String currency;
+
+    @SerializedName("price")
     String price;
 }
