1d3678aa4e5169d8b803dbe6a89743e306c4d633
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-05-10 19:07:59 +0300

    Initial commit

---
+++ app/src/main/java/com/yusuf/cryptocurrencyapp/Crypto.java
@@ -0,0 +1,7 @@
+package com.yusuf.cryptocurrencyapp;
+
+public class Crypto {
+
+    String currency;
+    String price;
+}
---
+++ app/src/main/java/com/yusuf/cryptocurrencyapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.yusuf.cryptocurrencyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //http://api.coinlayer.com/api/live?access_key=950eded663735093345be48d58ae6edb
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
